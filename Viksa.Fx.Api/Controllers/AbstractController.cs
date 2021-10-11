b4858e10f16a266bc0fafc8d6dd732e288ef7c3b
Viktor Safar <safar.viktor@gmail.com>
2021-10-11 20:07:53 +0200

    task 1c

---
+++ Viksa.Fx.Api/Controllers/AbstractController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Viksa.Fx.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class AbstractController : ControllerBase
+    {
+    }
+}
---
+++ Viksa.Fx.Api/Controllers/FxConversionsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Viksa.Fx.Business;
+
+namespace Viksa.Fx.Api.Controllers
+{
+    public class FxConversionsController : AbstractController
+    {
+        private readonly IFxBusiness _fxBusiness;
+
+        public FxConversionsController(IFxBusiness fxBusiness)
+        {
+            _fxBusiness = fxBusiness;
+        }
+
+        [Route("{fromCurrency}/{fromAmount}/{toCurrency}")]
+        [HttpGet]
+        public async Task<IActionResult> Convert([FromRoute] string fromCurrency, [FromRoute] decimal fromAmount, [FromRoute] string toCurrency)
+        {
+            var result = await _fxBusiness.GetToAmount(fromAmount, fromCurrency, toCurrency);
+            return Ok(result);
+        }
+    }
+}
